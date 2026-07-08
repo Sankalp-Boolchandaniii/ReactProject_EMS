5b6745372ecf54ae76fbb8d1c13e3d1e4809109b
Sankalp Boolchandani <boolchandanisankalp@gmail.com>
2026-07-09 01:33:04 +0530

    adding local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Login from "./components/Auth/Login";
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
 import AdminDashboard from "./components/Dashboard/AdminDashboard";
+import { setLocalStorage, getLocalStorage } from "./utils/localStorage";
 
 const App = () => {
+  useEffect(() => {
+    // setLocalStorage();
+    console.log(getLocalStorage());
+  });
   return (
     <div>
-      {/* <Login /> */}
+      <Login />
       {/* <EmployeeDashboard /> */}
       {/* <AdminDashboard /> */}
     </div>
